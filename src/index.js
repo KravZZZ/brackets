a3e3ff967a2bcc20e3af289380cb76c24c57e9f1
Pavel Krautsou <kravzzz.p@gmail.com>
2024-08-21 20:58:41 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const obj = {}
+
+  for(let i = 0; i < bracketsConfig.length; i++){
+    obj[bracketsConfig[i][1]] = bracketsConfig[i][0]
+  }
+
+  const stack = []
+
+  for(let i = 0; i < str.length; i++){
+    if(obj.hasOwnProperty(str[i])){
+
+      if(stack.length === 0 && obj[str[i]] !== str[i]){
+        return false
+      } else if(stack[stack.length - 1] ===  obj[str[i]]){
+        stack.pop()
+      } else if (obj[str[i]] === str[i]){
+        stack.push(str[i])
+      } else {
+        return false
+      }
+    } else {
+      stack.push(str[i])
+    }
+  }
+
+  return stack.length === 0
 }
+
